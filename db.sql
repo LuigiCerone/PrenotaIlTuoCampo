6d65f39fcdf608b1b0819b36a940fc4a8ef4915b
luigi.cerone <luigiceroneaq@gmail.com>
2018-02-06 17:39:45 +0100

    Foreign key added to db dump

--- db.sql
+++ db.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS tdwproject;
 CREATE DATABASE tdwproject;
 USE tdwproject;
 
@@ -6,9 +7,9 @@ CREATE TABLE availability
 (
   id         INT AUTO_INCREMENT
     PRIMARY KEY,
-  user_fk    INT REFERENCES user (id)    NOT NULL,
-  partner_fk INT REFERENCES partner (id) NOT NULL,
-  sport_fk   INT REFERENCES sport (id)   NOT NULL,
+  user_fk    INT REFERENCES user (id),
+  partner_fk INT REFERENCES partner (id),
+  sport_fk   INT REFERENCES sport (id),
   date       DATE                        NOT NULL,
   time       TIME                        NULL,
   busy       TINYINT(1) DEFAULT '0'      NOT NULL
@@ -20,37 +21,24 @@ CREATE TABLE award
 (
   id            INT AUTO_INCREMENT
     PRIMARY KEY,
-  name          VARCHAR(40)                    NOT NULL,
-  tournament_fk INT REFERENCES tournament (id) NOT NULL,
-  place         INT                            NOT NULL,
+  name          VARCHAR(40) NOT NULL,
+  tournament_fk INT REFERENCES tournament (id),
+  place         INT         NOT NULL,
   CONSTRAINT idx
   UNIQUE (tournament_fk, place)
 )
   ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS booking;
-CREATE TABLE booking
-(
-  id       INT AUTO_INCREMENT
-    PRIMARY KEY,
-  date     DATE                             NOT NULL,
-  time     TIME                             NOT NULL,
-  user_fk  INT REFERENCES user (id)         NULL,
-  field_fk INT REFERENCES field (id)        NULL,
-  approved TINYINT(1) DEFAULT '0'           NOT NULL,
-  valid    TINYINT(1) DEFAULT '1'           NOT NULL
-)
-  ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS field;
 CREATE TABLE field
 (
   id          INT AUTO_INCREMENT
     PRIMARY KEY,
-  partner_fk  INT REFERENCES partner (id)                                       NOT NULL,
-  sport_fk    INT REFERENCES sport (id)                                         NOT NULL,
+  partner_fk  INT REFERENCES partner (id),
+  sport_fk    INT REFERENCES sport (id),
   outdoor     TINYINT(1) DEFAULT '1'                                            NULL,
-  province_fk VARCHAR(2) REFERENCES province (sign)                             NOT NULL,
+  province_fk VARCHAR(2) REFERENCES province (sign),
   type        VARCHAR(40)                                                       NOT NULL,
   warmed      TINYINT(1) DEFAULT '0'                                            NOT NULL,
   number      INT                                                               NOT NULL
@@ -62,11 +50,11 @@ CREATE TABLE invitation
 (
   id              INT AUTO_INCREMENT
     PRIMARY KEY,
-  from_user_fk    INT REFERENCES user (id)                                                       NOT NULL,
-  to_user_fk      INT REFERENCES user (id)                                                       NOT NULL,
+  from_user_fk    INT REFERENCES user (id),
+  to_user_fk      INT REFERENCES user (id),
   date            TIMESTAMP DEFAULT CURRENT_TIMESTAMP                                            NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   status          TINYINT(1) DEFAULT '2'                                                         NOT NULL,
-  availability_fk INT REFERENCES availability (id)                                               NOT NULL
+  availability_fk INT REFERENCES availability (id)
 )
   ENGINE = InnoDB;
 
@@ -75,15 +63,16 @@ CREATE TABLE `match`
 (
   id             INT AUTO_INCREMENT
     PRIMARY KEY,
-  first_team_fk  INT REFERENCES team (id)                                  NOT NULL,
-  second_team_fk INT REFERENCES team (id)                                  NOT NULL,
-  tournament_fk  INT REFERENCES tournament (id)                            NOT NULL,
-  date           DATE                                                      NULL,
-  time           TIME                                                      NULL,
-  field_fk       INT REFERENCES field (id)                                 NULL,
-  day            INT                                                       NOT NULL
+  first_team_fk  INT REFERENCES team (id),
+  second_team_fk INT REFERENCES team (id),
+  tournament_fk  INT REFERENCES tournament (id),
+  date           DATE        NULL,
+  time           TIME        NULL,
+  field_fk       INT         NULL,
+  day            INT         NOT NULL
   COMMENT 'Giornata del campionato',
-  result         VARCHAR(20)                                               NULL
+  result         VARCHAR(20) NULL,
+  FOREIGN KEY (field_fk) REFERENCES field (id)
 )
   ENGINE = InnoDB;
 
@@ -92,10 +81,10 @@ CREATE TABLE news
 (
   id      INT AUTO_INCREMENT
     PRIMARY KEY,
-  title   VARCHAR(30)                   NULL,
-  date    DATE                          NULL,
-  text    TEXT                          NULL,
-  user_fk INT REFERENCES user (id)      NOT NULL
+  title   VARCHAR(30) NULL,
+  date    DATE        NULL,
+  text    TEXT        NULL,
+  user_fk INT REFERENCES user (id)
 )
   ENGINE = InnoDB;
 
@@ -128,8 +117,8 @@ CREATE TABLE ranking
 (
   id            INT AUTO_INCREMENT
     PRIMARY KEY,
-  tournament_fk INT REFERENCES tournament (id)         NOT NULL,
-  team_fk       INT REFERENCES team (id)               NOT NULL,
+  tournament_fk INT REFERENCES tournament (id),
+  team_fk       INT REFERENCES team (id),
   point         INT DEFAULT '0'                        NOT NULL
 )
   ENGINE = InnoDB;
@@ -139,23 +128,12 @@ CREATE TABLE resource
 (
   id         INT AUTO_INCREMENT
     PRIMARY KEY,
-  supply_fk  INT REFERENCES supply (id)          NOT NULL,
-  partner_fk INT REFERENCES partner (id)         NOT NULL,
+  supply_fk  INT REFERENCES supply (id),
+  partner_fk INT REFERENCES partner (id),
   available  TINYINT(1) DEFAULT '0'              NOT NULL
 )
   ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS review;
-CREATE TABLE review
-(
-  id       INT AUTO_INCREMENT
-    PRIMARY KEY,
-  user_fk  INT REFERENCES user (id)      NOT NULL,
-  text     VARCHAR(255)                  NOT NULL,
-  stars    INT                           NOT NULL,
-  approved TINYINT(1) DEFAULT '0'        NULL
-)
-  ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS sponsor;
 CREATE TABLE sponsor
@@ -170,10 +148,10 @@ CREATE TABLE sponsor
 DROP TABLE IF EXISTS sponsor2tournament;
 CREATE TABLE sponsor2tournament
 (
-  sponsor_fk    INT REFERENCES sponsor (id)    NOT NULL,
-  tournament_fk INT REFERENCES tournament (id) NOT NULL,
-  money         INT                            NOT NULL,
-  date          DATE                           NOT NULL,
+  sponsor_fk    INT REFERENCES sponsor (id),
+  tournament_fk INT REFERENCES tournament (id),
+  money         INT  NOT NULL,
+  date          DATE NOT NULL,
   CONSTRAINT idx
   UNIQUE (tournament_fk, sponsor_fk)
 )
@@ -204,11 +182,11 @@ CREATE TABLE team
 (
   id            INT AUTO_INCREMENT
     PRIMARY KEY,
-  name          VARCHAR(50)                     NOT NULL,
-  number        INT                             NOT NULL,
-  user_fk       INT REFERENCES user (id)        NOT NULL,
-  players       VARCHAR(255)                    NOT NULL,
-  tournament_fk INT REFERENCES tournament (id)  NOT NULL,
+  name          VARCHAR(50)  NOT NULL,
+  number        INT          NOT NULL,
+  user_fk       INT REFERENCES user (id),
+  players       VARCHAR(255) NOT NULL,
+  tournament_fk INT REFERENCES tournament (id),
   CONSTRAINT team_name_uindex
   UNIQUE (name)
 )
@@ -220,12 +198,12 @@ CREATE TABLE tournament
   id              INT AUTO_INCREMENT
     PRIMARY KEY,
   name            VARCHAR(40)                                        NOT NULL,
-  partner_fk      INT REFERENCES partner (id)                        NOT NULL,
+  partner_fk      INT REFERENCES partner (id),
   startDate       DATE                                               NOT NULL,
   endSubscription DATE                                               NULL,
   teamNumber      INT                                                NOT NULL,
   teamLeft        INT                                                NOT NULL,
-  sport_fk        INT REFERENCES sport (id)                          NOT NULL,
+  sport_fk        INT REFERENCES sport (id),
   scheduled       TINYINT(1) DEFAULT '0'                             NOT NULL
   COMMENT 'Se true significa che le partite sono state organizzate'
 )
@@ -234,10 +212,10 @@ CREATE TABLE tournament
 DROP TABLE IF EXISTS user;
 CREATE TABLE user
 (
-  firstName  VARCHAR(30)                 NOT NULL,
-  lastName   VARCHAR(30)                 NOT NULL,
   id         INT AUTO_INCREMENT
     PRIMARY KEY,
+  firstName  VARCHAR(30)                 NOT NULL,
+  lastName   VARCHAR(30)                 NOT NULL,
   email      VARCHAR(100)                NOT NULL,
   password   VARCHAR(30)                 NOT NULL,
   telnumber  VARCHAR(20)                 NULL,
@@ -249,3 +227,30 @@ CREATE TABLE user
 )
   ENGINE = InnoDB;
 
+DROP TABLE IF EXISTS booking;
+CREATE TABLE booking
+(
+  id       INT AUTO_INCREMENT
+    PRIMARY KEY,
+  date     DATE                             NOT NULL,
+  time     TIME                             NOT NULL,
+  user_fk  INT                              NULL,
+  field_fk INT                              NULL,
+  approved TINYINT(1) DEFAULT '0'           NOT NULL,
+  valid    TINYINT(1) DEFAULT '1'           NOT NULL,
+  FOREIGN KEY (user_fk) REFERENCES user (id),
+  FOREIGN KEY (field_fk) REFERENCES field (id)
+);
+
+DROP TABLE IF EXISTS review;
+CREATE TABLE review
+(
+  id       INT AUTO_INCREMENT
+    PRIMARY KEY,
+  user_fk  INT                           NULL,
+  text     VARCHAR(255)                  NOT NULL,
+  stars    INT                           NOT NULL,
+  approved TINYINT(1) DEFAULT '0'        NULL,
+  FOREIGN KEY (user_fk) REFERENCES user (id)
+)
+  ENGINE = InnoDB;
